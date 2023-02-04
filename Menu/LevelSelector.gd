8390a3e6953105a8422e1f3ea8fb51e8b61dd0a1
Daniel Faria <daniel.etitan@gmail.com>
2023-02-04 17:52:11 +0100

    Merge Menu, LevelSelector y Level Scenes, updates sprites

---
+++ Menu/LevelSelector.gd
@@ -0,0 +1,50 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+
+func _on_Level1_pressed():
+	get_tree().change_scene("res://Level.tscn")
+
+
+func _on_Level2_pressed():
+	get_tree().change_scene("res://Level.tscn")
+
+
+func _on_Level3_pressed():
+	get_tree().change_scene("res://Level.tscn")
+
+
+func _on_Level4_pressed():
+	get_tree().change_scene("res://Level.tscn")
+
+
+func _on_Level5_pressed():
+	get_tree().change_scene("res://Level.tscn")
+
+
+func _on_Level6_pressed():
+	get_tree().change_scene("res://Level.tscn")
+
+
+func _on_Level7_pressed():
+	get_tree().change_scene("res://Level.tscn")
+
+
+func _on_BackToMenu_pressed():
+	get_tree().change_scene("res://Menu.tscn")
